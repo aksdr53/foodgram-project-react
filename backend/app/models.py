9964d12f7e723e1561ddc878b453f59d0b818dbf
Александр Мыльников <ingaksdr@yandex.ru>
2023-07-28 23:10:06 +0300

    Start adding models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='reviews')
+    name = models.CharField(max_length=NAME_MAX_LENGTH,
+                            verbose_name='Имя',)
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год выпуска',
+        validators=(validate_year, ),
+    )
+    description = models.TextField(blank=True,
+                                   verbose_name='Описание')
+    genre = models.ManyToManyField(Genre,
+                                   related_name='genre',
+                                   verbose_name='Жанр')
+    category = models.ForeignKey(
+        Category, null=True,
+        on_delete=models.SET_NULL,
+        related_name='categories',
+        verbose_name='Категория',
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
---
+++ backend/users/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from foodgram.settings import (MAX_LENGTH,
+                                EMAIL_MAX_LENGTH)
+
+
+ADMIN = "admin"
+MODERATOR = "moderator"
+USER = "user"
+ROLE = [
+    (ADMIN, "Администратор"),
+    (MODERATOR, "Модератор"),
+    (USER, "Пользователь")
+]
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(default=USER,
+                            choices=ROLE,
+                            max_length=len(max(map(lambda x: x[0], ROLE),
+                                               key=len)))
+    email = models.EmailField(max_length=EMAIL_MAX_LENGTH,
+                              blank=False, unique=True)
+    username = models.CharField(blank=False,
+                                max_length=MAX_LENGTH, unique=True)
+    last_name = models.CharField(max_length=MAX_LENGTH, blank=True)
+    first_name = models.CharField(max_length=MAX_LENGTH, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+
+    def __str__(self) -> str:
+        return self.username
+
+    @property
+    def is_admin(self):
+        return (self.role == ADMIN
+                or self.is_superuser
+                or self.is_staff)
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    def __str__(self) -> str:
+        return self.username
