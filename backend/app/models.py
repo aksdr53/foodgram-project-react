d22dee66f0fb8705113b5a299dcd572b0ef31889
Александр Мыльников <ingaksdr@yandex.ru>
2023-08-10 11:33:28 +0300

    Add Recipe, Tag, Ingredient models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from users.models import User
+from foodgram.settings import NAME_MAX_LENGTH
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=NAME_MAX_LENGTH,
+                            verbose_name='Имя',)
+    color = models.CharField(max_length=16)
+    slug = models.CharField(max_length=NAME_MAX_LENGTH,
+                            verbose_name='Slug',)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=NAME_MAX_LENGTH,
+                            verbose_name='Имя',)
+    measurement_unit = models.CharField(max_length=16)
 
 
 class Recipe(models.Model):
@@ -7,25 +23,29 @@ class Recipe(models.Model):
                                related_name='reviews')
     name = models.CharField(max_length=NAME_MAX_LENGTH,
                             verbose_name='Имя',)
-    year = models.PositiveSmallIntegerField(
-        verbose_name='Год выпуска',
-        validators=(validate_year, ),
+    text = models.TextField(blank=True,
+                            verbose_name='Описание')
+    tags = models.ManyToManyField(Tag,
+                                  related_name='tags',
+                                  verbose_name='Тэги')
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        null=True,
+        default=None
     )
-    description = models.TextField(blank=True,
-                                   verbose_name='Описание')
-    genre = models.ManyToManyField(Genre,
-                                   related_name='genre',
-                                   verbose_name='Жанр')
-    category = models.ForeignKey(
-        Category, null=True,
-        on_delete=models.SET_NULL,
-        related_name='categories',
-        verbose_name='Категория',
+    cooking_time = models.IntegerField(validators=[MinValueValidator(1)],
+                                       verbose_name='Время приготовления')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name='ingredients',
+        verbose_name='Ингридиенты'
     )
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата публикации')
 
     class Meta:
-        verbose_name = 'Произведение'
-        ordering = ('name',)
+        verbose_name = 'Рецепты'
+        ordering = ('pub_date',)
 
     def __str__(self):
         return self.name
