b2f4f03a7d1890abf22549117d9e48ad791c8783
Александр Мыльников <ingaksdr@yandex.ru>
2023-08-28 19:46:26 +0300

    fixing font

--- backend/app/views.py
+++ backend/app/views.py
@@ -110,8 +110,8 @@ class RecipeViewSet(PermissionPolicyMixin, viewsets.ModelViewSet):
         )
         buffer = io.BytesIO()
         pdf_object = canvas.Canvas(buffer)
-        pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
-        pdf_object.setFont('Verdana', 14)
+        pdfmetrics.registerFont(TTFont('Vera'))
+        pdf_object.setFont('Vera', 14)
         pdf_object.drawCentredString(100, 800, "Список покупок")
         text_height = 700
         for ingredient in shopping_cart:
