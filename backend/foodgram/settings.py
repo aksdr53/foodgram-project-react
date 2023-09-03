3391a52a839ae6819186e83db6561c7df54201fb
Александр Мыльников <ingaksdr@yandex.ru>
2023-09-03 11:34:53 +0300

    Change db name

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram_db'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
