c3a795f9d751be71d9f03f8b55b21d868cde53af
Александр Мыльников <ingaksdr@yandex.ru>
2023-09-03 14:37:29 +0300

    Fixing validation5

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -145,6 +145,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
+        print('debug')
         print(ingredients)
         tags = validated_data.pop('tags')
         recipe = Recipe.objects.create(**validated_data)
